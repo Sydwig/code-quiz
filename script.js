b1d78774cce8b1262c1699191f3216e71d69a2a9
Exotic P <sjwiegman@gmail.com>
2022-08-23 19:59:24 -0400

    added showstate function to block the other sections display when not being used

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ var starter = document.querySelector("#start")
 var quizzer = document.querySelector("#quiz")
 var results = document.querySelector("#results")
 
+function showState() {
+    if(state === "start") {
+        starter.style.display = 'block';
+        quizzer.style.display = 'none';
+        results.style.display = 'none';
+    }
+    if(state === "quiz") {
+        starter.style.display = 'block';
+        quizzer.style.display = 'block';
+        results.style.display = 'none';
+        displayQuestion();
+    }
+    if(state === "results") {
+        starter.style.display = 'none';
+        quizzer.style.display = 'none';
+        results.style.display = 'block';
+    }
+};
+
+function init() {
+    showState();
+};
