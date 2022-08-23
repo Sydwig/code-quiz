9a2dad43f7e8d0df7ab1d1c0176ca261c33798a5
Exotic P <sjwiegman@gmail.com>
2022-08-23 19:53:10 -0400

    added query selectors to javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+var container = document.querySelector("#container");
+var starter = document.querySelector("#start")
+var quizzer = document.querySelector("#quiz")
+var results = document.querySelector("#results")
+
